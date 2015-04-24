9676f94af5540d363c7f2dd6d226ce1836793803
amardikian <alex.mardikian@gmail.com>
2015-04-24 14:45:41 -0700

    Array Sum Procedure

---
+++ Assembly Projects/Array Sum Procedure/Array Sum Procedure.asm
@@ -0,0 +1,43 @@
+; Array Sum Procedure.asm
+
+.386
+.model flat, stdcall
+.stack 4096
+ExitProcess PROTO, dwExitCode:DWORD
+
+.data
+array DWORD 1h, 20h, 300h, 4000h, 50000h
+.data?
+sum DWORD ?
+
+.code
+main PROC
+	mov esi, OFFSET array	; Store array location
+	mov ecx, LENGTHOF array	; Store number of elements in array
+	call ArraySum			; Sum will be stored in eax
+	mov sum, eax
+
+	INVOKE ExitProcess, 0
+main ENDP
+
+ArraySum PROC
+; Array location in esi and array length in ecx
+; Input array must be DWORD array
+	push esi	; Save esi
+	push ecx	; Save ecx
+
+	mov eax, 0	; Accumulator for sum
+
+L1:
+	add eax, [esi]
+	add esi, TYPE DWORD	; Reason for DWORD constraint on array
+	loop L1
+
+	pop esi		; Restore esi
+	pop ecx		; Restore ecx
+
+	ret
+ArraySum ENDP
+
+END main
+ 
